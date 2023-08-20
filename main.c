28a1e7e496449fb7ea68644ff3be638a68f740c6
Gabriel Gomes <gabegomes2020@gmail.com>
2023-08-20 14:34:11 -0300

    Apresentação

--- main.c
+++ main.c
@@ -1,6 +1,141 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 
-int main() {
-    printf("Hello, world!\n");
+/////
+// Assinatura das funções
+void tela_menu_principal(void);  // Adicione a assinatura da função aqui
+void tela_cadastrar_livro(void);
+void tela_gestao_estoque(void);
+void tela_registro_vendas(void);
+void tela_cadastrar_cliente(void);
+void tela_relatorios(void);
+
+/////
+// Programa principal
+int main(void) {
+    setlocale(LC_ALL, "Portuguese");
+    tela_menu_principal();
     return 0;
+}
+
+/////
+// Funções
+
+// Função de exibição do menu principal
+void tela_menu_principal(void) {
+    char op;
+    while (1) {
+        system("clear||cls");
+        puts("///////////////////////////////////////////////////////////////////////////////");
+        puts("///                                                                         ///");
+        puts("///            = = = = = = Sistema de Gestão para uma Livraria = = = = = =    ///");
+        puts("///                                                                         ///");
+        puts("///            1. Cadastro de Livros e Produtos                               ///");
+        puts("///            2. Gestão de Estoque                                          ///");
+        puts("///            3. Registro de Vendas de Livros                               ///");
+        puts("///            4. Cadastro de Clientes                                       ///");
+        puts("///            5. Relatórios                                                 ///");
+        puts("///            0. Sair                                                       ///");
+        puts("///                                                                         ///");
+        puts("///            Escolha a opção desejada: ");
+        scanf(" %c", &op);
+        getchar();
+
+        switch (op) {
+            case '1':
+                tela_cadastrar_livro();
+                break;
+            case '2':
+                tela_gestao_estoque();
+                break;
+            case '3':
+                tela_registro_vendas();
+                break;
+            case '4':
+                tela_cadastrar_cliente();
+                break;
+            case '5':
+                tela_relatorios();
+                break;
+            case '0':
+                exit(0);
+            default:
+                puts("Opção inválida. Pressione Enter para continuar...");
+                getchar();
+        }
+    }
+}
+
+
+// Função de cadastro de livros e produtos
+void tela_cadastrar_livro(void) {
+    system("clear||cls");
+    // ...
+    printf("///            = = = = = = = = Cadastrar Livro e Produto = = = = = = = =              ///\n");
+    // ...
+    printf("///            Título do livro:                                               ///\n");
+    printf("///            Autor:                                                        ///\n");
+    printf("///            Categoria:                                                    ///\n");
+    printf("///            Preço:                                                        ///\n");
+    printf("///            Quantidade em estoque:                                        ///\n");
+    // ...
+    printf("\nPressione Enter para voltar ao menu principal...");
+    getchar();
+}
+
+// Função de gestão de estoque
+void tela_gestao_estoque(void) {
+    system("clear||cls");
+    // ...
+    printf("///            = = = = = = = = Gestão de Estoque = = = = = = = = =              ///\n");
+    // ...
+    printf("///            Informe o código do livro/produto:                            ///\n");
+    printf("///            Nova quantidade em estoque:                                   ///\n");
+    // ...
+    printf("\nPressione Enter para voltar ao menu principal...");
+    getchar();
+}
+
+// Função de registro de vendas
+void tela_registro_vendas(void) {
+    system("clear||cls");
+    // ...
+    printf("///            = = = = = = = = Registro de Vendas de Livros = = = = = = = =              ///\n");
+    // ...
+    printf("///            Informe o código do livro vendido:                            ///\n");
+    printf("///            Quantidade vendida:                                           ///\n");
+    printf("///            Valor total da venda:                                         ///\n");
+    // ...
+    printf("\nPressione Enter para voltar ao menu principal...");
+    getchar();
+}
+
+// Função de cadastro de clientes
+void tela_cadastrar_cliente(void) {
+    system("clear||cls");
+    // ...
+    printf("///            = = = = = = = = Cadastrar Cliente = = = = = = = = =              ///\n");
+    // ...
+    printf("///            Nome completo do cliente:                                     ///\n");
+    printf("///            E-mail:                                                       ///\n");
+    printf("///            Telefone:                                                     ///\n");
+    // ...
+    printf("\nPressione Enter para voltar ao menu principal...");
+    getchar();
+}
+
+// Função de relatórios
+void tela_relatorios(void) {
+    system("clear||cls");
+    // ...
+    printf("///            = = = = = = = = Relatórios = = = = = = = = =              ///\n");
+    // ...
+    printf("///            1. Relatório de Livros em Estoque                            ///\n");
+    printf("///            2. Relatório de Vendas                                        ///\n");
+    printf("///            3. Relatório de Clientes                                      ///\n");
+    printf("///            0. Voltar ao menu anterior                                   ///\n");
+    // ...
+    printf("\nPressione Enter para voltar ao menu principal...");
+    getchar();
 }
