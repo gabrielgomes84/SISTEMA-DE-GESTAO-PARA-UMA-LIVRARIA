bfbdd6a4117e628266d115cddaaffaf75778fdff
Gabriel Gomes <gabegomes2020@gmail.com>
2023-10-10 21:23:37 -0300

    correções

--- cadastro_cliente.h
+++ cadastro_cliente.h
@@ -1,6 +1,16 @@
 #ifndef CADASTRO_CLIENTE_H
 #define CADASTRO_CLIENTE_H
 
+typedef struct {
+    char email[100];
+    char cpf[15];  // Tamanho típico de um CPF no Brasil
+    char nome[100];
+} Cliente;
+
 void tela_cadastrar_cliente(void);
+
+int validarEmail(const char *email);
+int validarCPF(const char *cpf);
+int validarCliente(Cliente cliente);
 
 #endif
